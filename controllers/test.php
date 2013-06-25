e3290d7be0781d3a83ed323794046c5cfb226d63
Ewan <ewan.duckworth@gmail.com>
2013-06-25 11:52:56 +0000

    Removed code used for testing calls via proxy

--- controllers/test.php
+++ controllers/test.php
@@ -24,9 +24,6 @@ class Test extends CI_Controller {
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, 0);
-        curl_setopt($ch, CURLOPT_PROXY, '192.168.3.3:8888');
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
                 'Content-Type: text/xml',
                 'Content-Length: ' . strlen($data_string))
